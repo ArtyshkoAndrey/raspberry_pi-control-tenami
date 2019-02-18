6f0d942d40ef08cf628fa30d44fab17c309e7a99
Fullion <artyshko.andrey@gmail.com>
2019-02-18 14:41:44 +0700

    Изменена функция для сбора данных с различных датчиков температуры

--- Temperature.py
+++ Temperature.py
@@ -6,19 +6,20 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+# device1_folder = base_dir + '28-000006af66d1' дом
+# device2_folder = base_dir + '28-000006dde2ec' Отопление
+
  
-def read_temp_raw():
+def read_temp_raw(device_file):
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
  
-def read_temp():
-    lines = read_temp_raw()
+def read_temp(device_folder):
+    device_file = base_dir + device_folder + '/w1_slave'
+    lines = read_temp_raw(device_file)
     while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
